028568ec0a9f20eabf6e14cfbfdec08781d2c6fc
Ёж <134137769+ssneandrei@users.noreply.github.com>
2026-08-22 00:42:19 +0300

    Refactor install.sh for architecture handling and download
    Updated architecture detection and download process for APK.

--- install.sh
+++ install.sh
@@ -1,43 +1,29 @@
 #!/bin/sh
-
-# 1. Определение архитектуры OpenWrt 25
-ARCH=$(apk info --print-arch)
-
+ARCH=$(uname -m)
 case "$ARCH" in
-  aarch64)
-    PKG_ARCH="aarch64"
-    ;;
-  mips* | mipsle*)
-    PKG_ARCH="mips"
-    ;;
-  x86_64)
-    PKG_ARCH="x86_64"
-    ;;
-  *)
-    echo "[-] Неподдерживаемая архитектура: $ARCH"
-    exit 1
-    ;;
+aarch64)
+APK_ARCH="aarch64"
+;;
+x86_64)
+APK_ARCH="x86_64"
+;;
+mips|mipsel)
+APK_ARCH="mips"
+;;
+*)
+echo "Неподдерживаемая архитектура: $ARCH"
+exit 1
+;;
 esac
-
-echo "[+] Определена архитектура: $PKG_ARCH"
-
-# 2. Получение ссылки на последний релиз с GitHub API
-DOWNLOAD_URL=$(wget -qO- https://api.github.com/repos/ssneandrei/happ_decoder_go/releases/latest | \
-  grep "browser_download_url" | \
-  grep "_${PKG_ARCH}\.apk" | \
-  cut -d '"' -f 4)
-
-if [ -z "$DOWNLOAD_URL" ]; then
-  echo "[-] Не удалось найти подходящий .apk пакет в последнем релизе."
-  exit 1
+echo "Обнаружена архитектура: $APK_ARCH"
+RELEASE_URL=$(wget -qO- https://api.github.com/repos/ssneandrei/happ_decoder_go/releases/latest | grep "browser_download_url.*.apk" | cut -d '"' -f 4)
+if [ -z "$RELEASE_URL" ]; then
+echo "Не удалось найти файл .apk в последнем релизе!"
+exit 1
 fi
-
-echo "[+] Скачивание: $DOWNLOAD_URL"
-wget -O /tmp/happ-decoder.apk "$DOWNLOAD_URL"
-
-# 3. Установка и очистка
-echo "[+] Установка пакета в OpenWrt..."
+echo "Загрузка пакета: $RELEASE_URL"
+wget -O /tmp/happ-decoder.apk "$RELEASE_URL"
+echo "Установка пакета..."
 apk add --allow-untrusted /tmp/happ-decoder.apk
 rm -f /tmp/happ-decoder.apk
-
-echo "[+] Установка успешно завершена! Настройки доступны в LuCI: Службы -> Happ Decoder"
+echo "Установка успешно завершена!"
